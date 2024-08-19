fbb740dd6972ee499a9927a57ae20201a474b9f2
ckim-csu <christopher.kim1@csuglobal.edu>
2024-08-18 23:14:13 -0600

    Add files via upload

---
+++ AddSubTwoNums.py
@@ -0,0 +1,13 @@
+# Script to add and subtract two inputted numbers
+
+# Get two numbers
+num1 = input("First number: ")
+num2 = input("Second number: ")
+
+# Get sum and difference of both numbers
+sum = float(num1) + float(num2)
+dif = float(num1) - float(num2)
+
+# Display results
+print("Sum: ", sum)
+print("Difference: ", dif)
---
+++ MultDivTwoNums.py
@@ -0,0 +1,13 @@
+# Script to multiply and divide two inputted numbers
+
+# Get two numbers
+num1 = input("First number: ")
+num2 = input("Second number: ")
+
+# Get sum and difference of both numbers
+product = float(num1) * float(num2)
+quotient = float(num1) / float(num2)
+
+# Display results
+print("Product: ", product)
+print("Quotient: ", quotient)
